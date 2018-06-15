8d876909e1039d921962f071bd6af1e0bb74d7ac
Jonny McFadden <jmcfadden1528@gmail.com>
2018-06-14 19:04:55 -0500

    Finished project

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -22,8 +22,8 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String index(Model model, @PathVariable("id") int id) {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
         Job job = jobData.findById(id);
@@ -68,7 +68,7 @@ public class JobController {
             int id = newJob.getId();
 
 
-            return "redirect:" + id;
+            return "redirect:?id=" + id;
 
     }
 }
